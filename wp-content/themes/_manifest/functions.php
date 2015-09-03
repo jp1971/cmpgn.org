9cfa7639b8707d502f2f8d53c78e602921d919b2
jp1971 <jameson@athleticsnyc.com>
2015-09-02 22:28:06 -0400

    Removing admin.php.

--- wp-content/themes/_manifest/functions.php
+++ wp-content/themes/_manifest/functions.php
@@ -7,7 +7,6 @@
 /**
  * Require custom theme functions
  */
-require_once( __DIR__ . '/functions/admin.php' );
 
 require_once( __DIR__ . '/functions/classes.php' );
 
@@ -28,58 +27,3 @@ require_once( __DIR__ . '/functions/images.php' );
 require_once( __DIR__ . '/functions/privacy.php' );
 
 require_once( __DIR__ . '/functions/utilities.php' );
-
-/**
- * Third Party Plugins
- */
-if ( 1 == get_option( 'manifest_acf_plugin' ) ) {
-
-	// Customize ACF path
-	add_filter( 'acf/settings/path', function ( $path ) {
-
-	    // update path
-	    $path = get_template_directory() . '/plugins/advanced-custom-fields-pro/';
-
-	    // return
-	    return $path;
-
-	});
-
-	// Customize ACF dir
-	add_filter( 'acf/settings/dir', function ( $dir ) {
-
-	    // update path
-	    $dir = get_template_directory_uri() . '/plugins/advanced-custom-fields-pro/';
-
-	    // return
-	    return $dir;
-
-	});
-
-	// Include ACF
-	require_once( __DIR__ . '/plugins/advanced-custom-fields-pro/acf.php' );
-
-}
-
-require_once( __DIR__ . '/plugins/timber-library/timber.php' );
-
-/**
- * Native Manifest Plugins
- */
-if ( 1 == get_option( 'manifest_cpss_plugin' ) ) {
-
-	require_once( __DIR__ . '/plugins/manifest-compatible-post-sharing-system/manifest-compatible-post-sharing-system.php' );
-
-}
-
-if ( 1 == get_option( 'manifest_docs_plugin' ) ) {
-
-	require_once( __DIR__ . '/plugins/manifest-docs/manifest-docs.php' );
-
-}
-
-if ( 1 == get_option( 'manifest_gallery_plugin' ) ) {
-
-	require_once( __DIR__ . '/plugins/manifest-gallery/manifest-gallery.php' );
-
-}
