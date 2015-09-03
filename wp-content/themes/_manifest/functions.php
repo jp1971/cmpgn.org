f41cb31e1930aaa5bc33f0beebd7244e42f2688a
jp1971 <jameson@athleticsnyc.com>
2015-09-02 22:44:43 -0400

    Requiring ACF Pro and Timber.

--- wp-content/themes/_manifest/functions.php
+++ wp-content/themes/_manifest/functions.php
@@ -25,3 +25,35 @@ require_once( __DIR__ . '/functions/images.php' );
 require_once( __DIR__ . '/functions/privacy.php' );
 
 require_once( __DIR__ . '/functions/utilities.php' );
+
+/**
+ * Require plugins
+ */
+
+// Customize ACF path
+add_filter( 'acf/settings/path', function ( $path ) {
+
+    // update path
+    $path = WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/';
+
+    // return
+    return $path;
+
+});
+
+// Customize ACF dir
+add_filter( 'acf/settings/dir', function ( $dir ) {
+
+    // update path
+    $dir = WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/';
+
+    // return
+    return $dir;
+
+});
+
+// Require ACF
+require_once( WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php' );
+
+// Require Timber
+require_once( WPMU_PLUGIN_DIR . '/timber-library/timber.php' );
